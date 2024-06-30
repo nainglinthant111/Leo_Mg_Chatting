c8c37738a2b3681964cc7d5af526ed01d827c1ea
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-06-30 12:11:47 +0630

    feat: auth ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import Login from '../app/auth/login/page'
+import Image from "next/image";
+// @ts-ignore
+import Logo from '../../public/logo.png'
+import AuthForm from "./components/AuthForm"
+
 const Page = () => {
     return (
-        <div className="">
-          <Login />
+        <div className="
+        flex
+        min-h-screen
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+        ">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Image src={Logo} alt="Logo" height={48} width={48} className="mx-auto w-auto"/>
+
+            </div>
+            <AuthForm />
         </div>
     );
 };
