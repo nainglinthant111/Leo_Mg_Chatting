d4bc6cbb60d7194eb6f2968d44cf15acb154d062
Minthantmg <minthantmg@gicjp.com>
2024-07-23 18:52:38 +0900

    fix(ui):update ui

--- src/app/components/Authentication.tsx
+++ src/app/components/Authentication.tsx
@@ -8,6 +8,7 @@ import {z} from "zod"
 import {zodResolver} from "@hookform/resolvers/zod";
 import AuthSocialButton from "@/app/components/AuthSocialButton";
 import {BsGithub} from "react-icons/bs";
+import {signIn} from "next-auth/react";
 
 const scheme = z.object({
     email: z.string().email(),
@@ -25,7 +26,7 @@ const Authentication = () => {
     const onSubmit: SubmitHandler<FormFields> = async (data) => {
         try {
             await new Promise((resolve) => setTimeout(resolve, 1000))
-            console.log(data)
+            sessionStorage.setItem('userData', JSON.stringify(data));
         } catch (error) {
             setError("root", {
                 message: "This email is already taken",
@@ -34,25 +35,29 @@ const Authentication = () => {
 
     }
 
+    const handleGoogleLogin = () => {
+        console.log("google login")
+    };
+
     return (
         <>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Sign in to your account
             </h2>
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            <div className="mt-8 mx-3 sm:mx-auto sm:w-full sm:max-w-md bg-white px-4 py-8 shadow rounded-lg sm:px-10">
                 <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
                         <Label htmlFor="email">Email</Label>
                         <Input {...register('email')} type="text" id="email"/>
                         {errors.email && (<div className="text-red-500">
-                            {errors.email.message}
+                            Please enter a valid email address.
                         </div>)}
                     </div>
                     <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
                         <Label htmlFor="password">Password</Label>
                         <Input {...register('password')} type="password" id="password"/>
                         {errors.password && (<div className="text-red-500">
-                            {errors.password.message}
+                            Password must be at least 8 characters long.
                         </div>)}
                     </div>
                     <Button disabled={isSubmitting} className="w-full mt-4">
@@ -74,7 +79,7 @@ const Authentication = () => {
                         </div>
                     </div>
                     <div className="mt-6 flex gap-2">
-                        <AuthSocialButton icon={BsGithub}/>
+                        <AuthSocialButton icon={BsGithub} onClick={handleGoogleLogin}/>
                     </div>
                     <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
                         <div>
